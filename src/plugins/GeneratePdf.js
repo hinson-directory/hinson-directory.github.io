0e217e38a6b19cfeb068130e0bd69ba5fe9e7143
Mason Fabel <mason.fabel@gmail.com>
2021-11-24 15:27:29 -0800

    Create sample pdf

--- src/plugins/GeneratePdf.js
+++ src/plugins/GeneratePdf.js
@@ -5,5 +5,28 @@ export default {
 		console.log(`GENERATE - ${sections.length} sections`);
 		console.log(config);
 		console.log(sections);
+
+		pdfMake.fonts = {
+			Roboto: {
+				normal: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Regular.ttf',
+				bold: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Medium.ttf',
+				italics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-Italic.ttf',
+				bolditalics: 'https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.66/fonts/Roboto/Roboto-MediumItalic.ttf'
+			},
+		};
+
+		const documentDefinition = {
+			defaultStyle: {
+				font: 'Roboto',
+			},
+			content: [
+				'This is a first paragraph.',
+				'This is a second paragraph.',
+				'This is a third paragraph. This is a third paragraph. This is a third paragraph. This is a third paragraph. This is a third paragraph. This is a third paragraph. This is a third paragraph. This is a third paragraph. This is a third paragraph. ',
+			],
+		};
+
+		pdfMake.createPdf(documentDefinition)
+			.open();
 	},
 };
